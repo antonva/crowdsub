30368a60d2dc596282e2ef546bca5a8a4bb22157
Snaebjorn <snaebjorn13@ru.is>
2014-05-08 17:33:28 +0000

    SubtitleController kominn inn

--- CrowdSub/Controllers/HomeController.cs
+++ CrowdSub/Controllers/HomeController.cs
@@ -19,136 +19,7 @@ namespace CrowdSub.Controllers
             return View();
         }
 
-		public ActionResult search_for_title_or_user(string search_query) //View done
-		{
-			//search database for both user and title, populate list with corresponding results
-			//send list to view as model, add razor declaration at the top
-
-			return View();
-		}
-
-
-		[HttpGet]
-		public ActionResult video_profile(int? video_id) //nullable id  //View done
-		{
-			//get title from video table, create video_profile_model
-			//with the correct data and send it to the view
-			//operations (buttons on the page):
-			//edit subtitle
-			//upload subtitle
-			//download subtitle
-			//export as different format (maybe at the moment)
-			//request subtitle
-
-			return View();
-		}
-
-		public ActionResult edit_subtitle(int? subtitle_id) //nullable id
-		{
-			//get subtitle from subtitle table, create edit_subtitle_model
-			//with the correct data and send it to the view
-			//operations:
-			//save changes
-			//submit comment
-
-			return View();
-		}
-
-		public ActionResult upload_subtitle(int? video_id) //nullable id
-		{
-			//get video from video table, send title to view as a string
-			//maybe show poster?
-			//allow user to write in the version
-			//browse for SRT file OR write subtitle in textedit (maybe at the moment)
-			//press upload button, if subtitle exists for version
-			//then this file will become the current version, else it will
-			//become the first version
-
-			return View();
-		}
-
-		[HttpGet]
-		public ActionResult request_subtitle()
-		{
-			return View();
-		}
-
-		[HttpPost]
-		public ActionResult request_subtitle(int? video_id) //nullable id
-		{
-			//get video from video table, send title to view as a string
-			//maybe show poster?
-			//allow user to select version
-			//then press submit
-
-			return View();
-		}
-
-		[HttpGet]
-		public ActionResult get_top_downloads()
-		{
-			//get 10 most downloaded subtitle files
-			//using LINQ query
-
-			return View();
-		}
-
-		[HttpGet]
-		public ActionResult get_top_requests()
-		{
-			//get 10 most upvoted requests from database
-			//using LINQ query
-
-			return View();
-		}
-
-		[HttpGet]
-		public ActionResult get_recent_uploads()
-		{
-			//get newest subtitles
-			//using LINQ query
-
-			return View();
-		}
-
-		[HttpGet]
-		public ActionResult get_all_subtitles()
-		{
-			//get all subtitles
-			//using LINQ query
-			//maybe use paging?
-
-			return View();
-		}
-
-		[HttpGet]
-		public ActionResult get_all_requests()
-		{
-			//get all requests
-			//using LINQ query
-			//maybe use paging?
-
-			return View();
-		}
-
-		[HttpGet]
-		public ActionResult create_video_profile()
-		{
-			return View();
-		}
-
-		[HttpPost]
-		public ActionResult create_video_profile(FormCollection form)
-		{
-			//user fills out form which contains
-			//title, type (DropDown), year (DropDown),
-			//imdb URL (auto fill-in maybe?), spoken language (DropDown)
-			//create button will submit that shit
-
-			//redirect user to the video profile he created
-			return RedirectToAction("show_video_profile"); //add video id as second parameter
-		}
-
+        [HttpGet]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -156,8 +27,7 @@ namespace CrowdSub.Controllers
             return View();
         }
 
-
-
+        [HttpGet]
         public ActionResult Users()
         {
             CrowdSub.Models.user u = new CrowdSub.Models.user();
@@ -184,6 +54,7 @@ namespace CrowdSub.Controllers
 			return View();
 		}
 
+		[HttpGet]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
