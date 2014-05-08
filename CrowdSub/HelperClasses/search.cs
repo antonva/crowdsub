1b761ddae4213f99282c4bff2acc9fd2b3d764c9
Snaebjorn <snaebjorn13@ru.is>
2014-05-08 16:41:27 +0000

    setti upp search hjálparklasa og færði gamla homeController yfir

---
+++ CrowdSub/HelperClasses/search.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CrowdSub.HelperClasses
+{
+	public class search
+	{
+		private static search the_instance = null;
+
+		public static search instance //SINGLETON PATTERN
+		{
+			get
+			{
+				if(the_instance == null)
+				{
+					the_instance = new search();
+				}
+				return the_instance;
+			}
+		}
+
+		//TODO: write function to search for video and another one to search for user
+	}
+}
