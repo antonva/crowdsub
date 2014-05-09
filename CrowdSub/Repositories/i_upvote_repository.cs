fb936d36a7f1eda13de1df25ad37d491ad597553
ArnarDori <arnara13@ru.is>
2014-05-09 16:53:43 +0000

    Bjó til upvote_repository.cs  + i_upvote_repository.cs  inn í repositories

---
+++ CrowdSub/Repositories/i_upvote_repository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrowdSub.Repositories
+{
+    interface i_upvote_repository
+    {
+    }
+}
---
+++ CrowdSub/Repositories/upvote_repository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CrowdSub.Models;
+
+namespace CrowdSub.Repositories
+{
+    public class upvote_repository : i_upvote_repository
+    {
+
+    }
+}
