50cb4cce5537df01f084ccde6ab1e87e0043eb98
gmarteinsson <gmarteinsson@gmail.com>
2014-05-09 19:26:07 +0000

    Egils Gull

---
+++ CrowdSub.Tests/Mocks/mock_request_comments_repository.cs
@@ -0,0 +1,32 @@
+﻿using CrowdSub.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrowdSub.Tests.Mocks
+{
+    class mock_request_comments_repository : i_request_comments_repository
+    {
+        public IQueryable<Models.request> get_comment()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool get_comment_by_id()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool edit_comment_by_id()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool delete_comment_by_id()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
