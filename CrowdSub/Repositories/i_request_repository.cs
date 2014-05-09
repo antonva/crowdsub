4d500e643c236843b151cfef8f50df7e8322ee43
omaroskars <omaroskars15@gmail.com>
2014-05-09 16:11:23 +0000

    request_repository & i_request repository added

---
+++ CrowdSub/Repositories/i_request_repository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CrowdSub.Models;
+
+namespace CrowdSub.Repositories
+{
+    public interface i_request_repository
+    {
+        IQueryable<request> get_requests();
+    }
+}
