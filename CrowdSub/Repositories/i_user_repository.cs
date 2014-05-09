a991564d8df9fe0c4d47b63d683900eb1c28a020
Anton Vilhelm Ásgeirsson <anton.v.asgeirsson@gmail.com>
2014-05-09 15:28:57 +0000

    Readded stuff to csproj

---
+++ CrowdSub/Repositories/i_user_repository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CrowdSub.Models;
+
+namespace CrowdSub.Repositories
+{
+    public interface i_user_repository
+    {
+        //TODO: connect to database
+
+        public IQueryable<user> get_subtitles()
+        {
+            //just for testing..
+            return null;
+        }
+    }
+}
