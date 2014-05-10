42a26c9c21b6a8c9800c6c727723d0332da0a49f
gmarteinsson <gmarteinsson@gmail.com>
2014-05-10 11:29:55 +0000

    mock_request_comments commented out

--- CrowdSub.Tests/Mocks/mock_request_comments_repository.cs
+++ CrowdSub.Tests/Mocks/mock_request_comments_repository.cs
@@ -11,26 +11,26 @@ namespace CrowdSub.Tests.Mocks
     class mock_request_comments_repository : i_request_comments_repository
     {
         //TODO: connect to database
-        private crowddbEntities db = new crowddbEntities();
+        
 
-        public IQueryable<Models.request> get_comment()
-        {
-            throw new NotImplementedException();
-        }
+        //public IQueryable<Models.request> get_comment()
+        //{
+        //    throw new NotImplementedException();
+        //}
 
-        public bool get_comment_by_id()
-        {
-            throw new NotImplementedException();
-        }
+        //public bool get_comment_by_id()
+        //{
+        //    throw new NotImplementedException();
+        //}
 
-        public bool edit_comment_by_id()
-        {
-            throw new NotImplementedException();
-        }
+        //public bool edit_comment_by_id()
+        //{
+        //    throw new NotImplementedException();
+        //}
 
-        public bool delete_comment_by_id()
-        {
-            throw new NotImplementedException();
-        }
+        //public bool delete_comment_by_id()
+        //{
+        //    throw new NotImplementedException();
+        //}
     }
 }
