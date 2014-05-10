010f59f714d8deffaf70e0299ecf0cc10c45e058
ArnarDori <arnara13@ru.is>
2014-05-10 11:57:29 +0000

    Dóra klasar villulausir 2

---
+++ CrowdSub.Tests/Mocks/mock_subtitle_comments_repository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CrowdSub.Repositories;
+using CrowdSub.Models;
+
+namespace CrowdSub.Tests.Mocks
+{
+    class mock_subtitle_comments_repository : i_subtitle_comments_repository
+    {
+    }
+}
---
+++ CrowdSub.Tests/Mocks/mock_upvote_repository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CrowdSub.Repositories;
+using CrowdSub.Models;
+
+namespace CrowdSub.Tests.Mocks
+{
+    class mock_upvote_repository : i_upvote_repository
+    {
+        public IQueryable<upvote> get_upvote()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int get_upvotes_by_requestid()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool create_upvote()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool has_user_upvoted()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool delete_upvote()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int get_upvotes_by_userid()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
