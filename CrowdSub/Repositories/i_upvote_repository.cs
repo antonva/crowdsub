c80c1bccc8a5febfd1213cdc8037dbbe77dca287
ArnarDori <arnara13@ru.is>
2014-05-10 11:54:50 +0000

    i_upvote_repository interface villulaust

--- CrowdSub/Repositories/i_upvote_repository.cs
+++ CrowdSub/Repositories/i_upvote_repository.cs
@@ -17,12 +17,12 @@ namespace CrowdSub.Repositories
 
         //If the user has upvoted the subtitle, can he downvoted (unlike) it?
         //Takes in parameter for request id and user id
-        bool has_user_upvote();
+        bool has_user_upvoted();
         
         bool delete_upvote();
 
         //Possibly a function which gets all the upvotes which the user has made
-        //int get_upvotes_by_userid();
+        int get_upvotes_by_userid();
 
     }
 }
