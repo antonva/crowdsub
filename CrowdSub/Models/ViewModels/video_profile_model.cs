fee5fb6be7852bcf2da19ab1e22b275d2cd6a966
omaroskars <omaroskars15@gmail.com>
2014-05-12 21:02:19 +0000

    created profile model

---
+++ CrowdSub/Models/ViewModels/video_profile_model.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CrowdSub.Models
+{
+    public class video_profile_model
+    {
+        public video video;
+        public IEnumerable<subtitle> subtitles;
+    }
+}
