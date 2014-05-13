9fff47e7eea4236c700d35691527b65190108805
Snaebjorn <snaebjorn13@ru.is>
2014-05-13 13:30:59 +0000

    Created a new project, Code First

--- CrowdSub/Models/CrowdSubModels.Context.cs
+++ CrowdSub/Models/CrowdSubModels.Context.cs
@@ -31,5 +31,10 @@ namespace CrowdSub.Models
         public virtual DbSet<user> users { get; set; }
         public virtual DbSet<video> videos { get; set; }
         public virtual DbSet<request> requests { get; set; }
+        public virtual DbSet<C__MigrationHistory> C__MigrationHistory { get; set; }
+        public virtual DbSet<AspNetRole> AspNetRoles { get; set; }
+        public virtual DbSet<AspNetUserClaim> AspNetUserClaims { get; set; }
+        public virtual DbSet<AspNetUserLogin> AspNetUserLogins { get; set; }
+        public virtual DbSet<AspNetUser> AspNetUsers { get; set; }
     }
 }
