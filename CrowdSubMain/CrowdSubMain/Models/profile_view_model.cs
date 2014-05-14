2b8487f06ea3873b61732a134e88732f4d58f1ae
Bjornhall <bjorn@wedo.is>
2014-05-14 16:08:01 +0000

    Video Profile listi af subtitles

---
+++ CrowdSubMain/CrowdSubMain/Models/profile_view_model.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CrowdSubMain.Models;
+using CrowdSubMain.DAL;
+
+namespace CrowdSubMain.Models
+{
+    public class profile_view_model
+    {
+        public video video { get; set; }
+        public IEnumerable<subtitle> subtitles { get; set; }
+    }
+}
