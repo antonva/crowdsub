428d5676852cab495c9368621a65ee3f3dd28e44
gmarteinsson <gmarteinsson@gmail.com>
2014-05-16 09:34:13 +0000

    FilterConfig, VideoController, Error handling

--- CrowdSubMain/CrowdSubMain/App_Start/FilterConfig.cs
+++ CrowdSubMain/CrowdSubMain/App_Start/FilterConfig.cs
@@ -3,11 +3,13 @@ using System.Web.Mvc;
 
 namespace CrowdSubMain
 {
-	public class FilterConfig
-	{
-		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
-		{
-			filters.Add(new HandleErrorAttribute());
-		}
-	}
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());  
+        }
+    }
 }
+
+
