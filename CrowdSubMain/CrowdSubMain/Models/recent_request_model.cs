a058b2816ee166d7f91551b51554a4a38cc172c4
Bjornhall <bjorn@wedo.is>
2014-05-16 15:52:54 +0000

    Recent, top shizznit

---
+++ CrowdSubMain/CrowdSubMain/Models/recent_request_model.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CrowdSubMain.Models
+{
+    public class recent_request_model
+    {
+        public request request { get; set; }
+        public video video { get; set; }
+    }
+}
