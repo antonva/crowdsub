d86da49a3bf832e5f3952fd38c0ff268f53ab8da
omaroskars <omaroskars15@gmail.com>
2014-05-16 04:20:55 +0000

    Searchbar with actions

---
+++ CrowdSubMain/CrowdSubMain/Models/video_language_search.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CrowdSubMain.Models;
+using CrowdSubMain.DAL;
+
+namespace CrowdSubMain.Models
+{
+    public class video_language_search
+    {
+        //public IEnumerable<subtitle> subtitles { get; set; }
+        //public IEnumerable<video> videos { get; set; }
+        public IEnumerable<search_pair> search_pairs { get; set; }
+    }
+}
